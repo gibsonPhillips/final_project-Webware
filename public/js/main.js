82014d31008fce6807e46d1e70965e2bb55fbf9c
DireLupus810 <jon@teamasher.net>
2024-10-03 20:09:20 -0400

    implemented preliminary express server and reorganized things so the server would stop having a heart attack (If I reorganized things bady, feel free to re-rearrange)

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+'use strict'
+
+function on_press() {
+    console.log("Hello World!")
+}
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express"),
 
+app = express()
+
+app.use(express.static("public"))
+
+app.use(express.json())
+
+app.listen(3000)
