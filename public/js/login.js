96e85fe1402f7471803817f347368e2574278fc7
DireLupus810 <jon@teamasher.net>
2024-10-05 22:34:03 -0400

    Starter login scaffolding done, just need database info. Cookies are working too!

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+let counter = 0;
+
+async function attempt_login () {
+    console.log(counter);
+    let result = await fetch("/login_request", { method: "POST" });
+    
+    window.location.href = result.url;
+} 
+
+window.onload = async function () {
+    let login_button = document.getElementById("login-button");
+    
+    login_button.onclick = attempt_login;
+}
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const cookie = require("cookie-session");
+const path = require("path");
 
-app = express()
+app = express();
+
+app.use(
+    cookie({
+      name: "session",
+      keys: ["verysecurekey(noitsnot)", "itssupersecureipromise"],
+    })
+);
 
 app.use(express.static("./public"));
+
+app.post("/login_request", async function (req, res) {
+    console.log(req.session.login);
+    req.session.login = true;
+    res.redirect("index.html");
+})
 
 app.use(express.json())
 
